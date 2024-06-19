9331b55490d8c32d35cdb49bd31141b55277b329
SenaBel <abelsena1@hotmail.com>
2024-06-19 09:37:25 -0300

    feat(creation of project )

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+//import "./App.css";
+import Form from "./Components/Form/Form";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-red-600">Inscreva - se</h1>
+      <Form />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/Form/Form.tsx
@@ -0,0 +1,9 @@
+function Form() {
+  return (
+    <div>
+      <input type="text" />
+    </div>
+  );
+}
+
+export default Form;
